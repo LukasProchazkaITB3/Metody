cf5a0b24904e85e20716acdcdc9d21cfb1c84296
Lycky_LITH <prochazkal.03@spst.eu>
2021-03-09 19:08:13 +0100

    Metody04

--- Metody/Form1.cs
+++ Metody/Form1.cs
@@ -17,9 +17,26 @@ namespace Metody
             InitializeComponent();
         }
 
-        private double Vzdalenost()
+        private double Vzdalenost(double aX, double aY, double bX, double bY)
+        {
+            double x = Math.Abs(aX - bX);
+            double y = Math.Abs(aY - bY);
 
+            double vysl = Math.Sqrt((x * x) + (y * y));
+
+            return vysl;
+        }
         private void button1_Click(object sender, EventArgs e)
+        {
+            double aX = Convert.ToInt32(textBox1.Text.ToString());
+            double aY = Convert.ToInt32(textBox2.Text.ToString());
+            double bX = Convert.ToInt32(textBox3.Text.ToString());
+            double bY = Convert.ToInt32(textBox4.Text.ToString());
+
+            label1.Text = Vzdalenost(aX, aY, bX, bY).ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
         {
 
         }
